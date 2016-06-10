1db808c18da250abefa15d280eea4779d93aa4ca
azuer88 <blue.cuenca@gmail.com>
2016-06-10 12:54:26 +0800

    add other JS dev dependencies
    Signed-off-by: azuer88 <blue.cuenca@gmail.com>

--- src/janus/janus/settings.py
+++ src/janus/janus/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'debug_toolbar',
+    'webpack_loader',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -123,3 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'janus/static'),
+]
