10a6a6e7b4a08b777609c4c136fd1bdbd1b9ea1d
azuer88 <blue.cuenca@gmail.com>
2016-06-13 15:44:21 +0800

    add react-hotloading
    Signed-off-by: azuer88 <blue.cuenca@gmail.com>

--- src/assets/js/app2.jsx
+++ src/assets/js/app2.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 module.exports = React.createClass({
     render: function() {
-        return <h3>Another react component</h3>
+        return <h3>Another react component -- realtime</h3>
     }
 });
 
--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,26 @@ var BundleTracker = require('webpack-bundle-tracker')
 module.exports = {
   context: __dirname,
 
-  entry: './src/assets/js/index', // entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
+  entry: [
+      // './src/assets/js/index', // entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
+      'webpack-dev-server/client?http://localhost:3000',
+      'webpack/hot/only-dev-server',
+      './src/assets/js/index'
+  ],
 
   output: {
       path: path.resolve('./src/assets/bundles/'),
       filename: "[name]-[hash].js",
+      publicPath: 'http://localhost:3000/assets/bundles/', // tell django to use thisURL to load packages and not use STATIC_URL + bundle_name
+          
   },
 
+  resolveLoader: {
+      root: path.join(__dirname, "node_modules")
+  },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(), // don't reload if there is an error
     new BundleTracker({filename: './webpack-stats.json'}),
   ],
 
@@ -21,8 +33,10 @@ module.exports = {
       { 
           test: /\.jsx?$/, 
           exclude: /node_modules/, 
-          loader: 'babel-loader',
-          query: { presets: ['react'] } 
+          loaders: [
+              'react-hot', 
+              'babel-loader?presets[]=react'
+              ],
       }, // to transform JSX into JS
     ],
   },
