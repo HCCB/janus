e5eacd7df1874cbff58136613ea0bb41a476d1c2
azuer88 <blue.cuenca@gmail.com>
2016-06-15 15:13:35 +0800

    add reactjs interactivity using nav
    Signed-off-by: azuer88 <blue.cuenca@gmail.com>

--- src/assets/js/app2.jsx
+++ src/assets/js/app2.jsx
@@ -1,22 +1,26 @@
 var React = require('react');
 var Alert = require('react-bootstrap/lib/Alert');
 var Button = require('react-bootstrap/lib/Button');
+var Nav = require('react-bootstrap/lib/Nav');
+var NavItem = require('react-bootstrap/lib/NavItem');
 
 var ipsumText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.';
 
 module.exports = React.createClass({
-    doClick: function() {
-        alert("you clicked.");
+    getInitialState: function() {
+        return ({ activeKey: 1 });
+    },
+    handleSelect: function(selectedKey) {
+        alert("you clicked." + selectedKey);
+        this.setState({activeKey: selectedKey});
     },
     render: function() {
         return (
-            <div>
-                <h3>Another react component -- realtime</h3>
-                <Button bsStyle="primary" onClick={this.doClick}>
-                    Button
-                </Button><br/>
-                <div>{ ipsumText }</div>
-            </div>
+            <Nav bsStyle="pills" activeKey={this.state.activeKey} onSelect={this.handleSelect}>
+                <NavItem eventKey={1} href="/home" title="Home">Home</NavItem>
+                <NavItem eventKey={2} href="/patents" title="Patients">Patient</NavItem>
+                <NavItem eventKey={4} href="/tests" title="Tests">Tests</NavItem>
+            </Nav>
         )
     }
 });
