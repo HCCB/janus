a1c8fc4ec681ac0f0803ff674576262943b60d8d
azuer88 <blue.cuenca@gmail.com>
2016-07-02 13:22:01 +0800

    change djang-rest-framework default authorization permission level to authenticated only
    Signed-off-by: azuer88 <blue.cuenca@gmail.com>

--- src/janus/janus/settings.py
+++ src/janus/janus/settings.py
@@ -165,7 +165,7 @@ if not DEBUG:
 
 # Django REST Framework Defaults and Settings:
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
     'PAGE_SIZE': 10,
 }
 
