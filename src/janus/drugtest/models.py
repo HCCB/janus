a5ed720d781465e31cbba639dc7f74f03e2c4a78
azuer88 <blue.cuenca@gmail.com>
2016-07-05 13:17:55 +0800

    install django-tinymce
    Signed-off-by: azuer88 <blue.cuenca@gmail.com>

--- src/janus/drugtest/models.py
+++ src/janus/drugtest/models.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
+from datetime.date import today
+
 from django.db import models
 
 
@@ -48,7 +50,7 @@ class Patient(Person):
 
 class Physician(Person):
     def __unicode__(self):
-        return "Dr. %s" % self.family_name
+        return u"Dr. %s" % self.family_name
 
 
 class Staff(Person):
@@ -64,3 +66,56 @@ class Staff(Person):
             self._get_mi(),
             self.family_name,
             self._get_suffix())
+
+
+class TestCategory(models.Model):
+    name = models.CharField(max_length=60)
+    alternate_name = models.CharField(max_length=60, blank=True, default='')
+
+    def __unicode__(self):
+        if self.alternate_name:
+            return u"%s (%s)" % (self.name, self.alternate_name)
+        else:
+            return u"%s" % self.name
+
+RESULT_TYPE_CHOICE = (
+    (1, 'Numeric'),
+    (2, 'Text'),
+)
+
+
+class Analysis(models.Model):
+    category = models.ForeignKey(TestCategory)
+    name = models.CharField(max_length=60)
+    short_name = models.CharField(max_length=30)
+    result_type = models.SmallIntegerField(choides=RESULT_TYPE_CHOICE)
+    reference_text = models.TextField()
+
+    def __unicode__(self):
+        return u"%s" % self.short_name
+
+
+class ResultMaster(models.Model):
+    patient = models.ForeignKey(Patient)
+    case_number = models.CharField(max_legnth=30)
+    room_number = models.CharField(max_length=20)
+    date = models.DateField(default=today)
+    category = models.ForeignKey(TestCategory)
+
+    def __unicode__(self):
+        return u"%s #: %s - %s" % (
+            self.patient.fullname,
+            self.case_number,
+            self.category)
+
+
+class ResultDetail(models.Model):
+    master = models.ForeignKey(ResultMaster)
+    analysis = models.ForeignKey(Analysis)
+    result = models.TextField()
+
+    def __unicode__(self):
+        return u"%s: %s" % (
+            self.analysis,
+            self.result,
+        )
