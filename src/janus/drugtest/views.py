fcc383caab924906437a675b093ee5c5b09a9585
azuer88 <blue.cuenca@gmail.com>
2016-07-11 09:16:28 +0800

    Test PDF generation
    Signed-off-by: azuer88 <blue.cuenca@gmail.com>

--- src/janus/drugtest/views.py
+++ src/janus/drugtest/views.py
@@ -1,7 +1,9 @@
 # from django.shortcuts import render
+from django.http import HttpResponse
 
 from rest_framework import viewsets
 # , permissions
+from reportlab.pdfgen import canvas
 
 from serializers import PatientSerializer
 from models import Patient
@@ -13,3 +15,17 @@ class PatientViewSet(viewsets.ModelViewSet):
     """
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
+
+
+def pdf_view(request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'filename="report.pdf"'
+    p = canvas.Canvas(response)
+    p.init_graphics_state()
+
+    p.drawString(100, 100, "Hello world.")
+
+    p.showPage()
+    p.save()
+
+    return response
