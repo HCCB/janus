6949f1df7dde08c50de67a2bbb448ab6ef227bf1
azuer88 <blue.cuenca@gmail.com>
2016-07-15 17:01:39 +0800

    add master info to pdf sample report template
    Signed-off-by: azuer88 <blue.cuenca@gmail.com>

--- src/janus/report/report.py
+++ src/janus/report/report.py
@@ -10,12 +10,14 @@ from reportlab.platypus.frames import Frame
 from reportlab.lib.units import cm
 from reportlab.lib.pagesizes import A5, landscape
 
+from reportlab.platypus.tables import Table, TableStyle
+
 from reportlab.lib.enums import TA_CENTER
 
 from reportlab.lib.utils import ImageReader
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.lib.colors import red
+from reportlab.lib.colors import black, red
 
 # from reportlab.platypus.flowables import Flowable
 
@@ -26,24 +28,29 @@ class ReportTemplate(BaseDocTemplate):
 
     def __init__(self, filename, **kw):
         self.allowSplitting = 0
+
+        self.pagesize = kw.get('pagesize', landscape(A5))
+        kw['pagesize'] = self.pagesize
+
         BaseDocTemplate.__init__(self, filename, **kw)
 
-        self.actualWidth, self.actualHeight = landscape(A5)
-        print "self.height:", self.height
-        self.topmargin = 2.85 * cm
-        self.leftmargin = 0.15 * cm
-        self.bottommargin = 0.15*cm
+        w, h = self.pagesize
 
-        fh = self.actualHeight - self.topmargin - self.bottommargin
-        fw = self.actualWidth - (2 * self.leftmargin)
+        # self.actualWidth, self.actualHeight = landscape(A5)
+        self.topMargin = 2.85 * cm
+        self.leftMargin = 0.15 * cm
+        self.bottomMargin = 0.15 * cm
 
-        frame = Frame(self.leftmargin, self.bottommargin,
+        fh = h - self.topMargin - self.bottomMargin
+        fw = w - (2 * self.leftMargin)
+
+        frame = Frame(self.leftMargin, self.bottomMargin,
                       fw, fh,
                       id='ContentFrame',
                       showBoundary=True)
 
         template = PageTemplate('normal', frames=[frame, ],
-                                pagesize=landscape(A5))
+                                pagesize=self.pagesize)
         self.loadFonts()
         self.addPageTemplates(template)
 
@@ -67,9 +74,9 @@ class ReportTemplate(BaseDocTemplate):
     def drawLogo(self):
         logo_fname = self.normpath('static/HCCB-Hospital-Logo.png')
         logo = ImageReader(logo_fname)
-        h = self.actualHeight
-        self.canv.drawImage(logo, 0.2*cm, h-self.topmargin,
-                            height=self.topmargin,
+        w, h = self.pagesize
+        self.canv.drawImage(logo, 0.2*cm, h-self.topMargin,
+                            height=self.topMargin,
                             mask='auto',
                             preserveAspectRatio=True
                             )
@@ -101,16 +108,15 @@ class ReportTemplate(BaseDocTemplate):
         story.append(Paragraph("Telefax No.: +63 (95) 342-397/225-6872", sN))
         story.append(Paragraph("email: hccb.hospital@gmail.com", sN))
 
-        w = self.actualWidth
-        h = self.actualHeight
-        f = Frame(0, h-self.topmargin, w, self.topmargin,
+        w, h = self.pagesize
+        f = Frame(0, h-self.topMargin, w, self.topMargin,
                   showBoundary=False)
         f.addFromList(story, self.canv)
 
         self.canv.setStrokeColor(red)
         self.canv.setLineWidth(3)
-        self.canv.line(0.15*cm, h-self.topmargin,
-                       w-0.15*cm, h-self.topmargin,
+        self.canv.line(0.15*cm, h-self.topMargin,
+                       w-0.15*cm, h-self.topMargin,
                        )
 
     def normpath(self, filespec):
@@ -120,6 +126,54 @@ class ReportTemplate(BaseDocTemplate):
     def loadFonts(self):
         ttfFile = self.normpath('fonts/oldeng.ttf')
         pdfmetrics.registerFont(TTFont("OldEngMT", ttfFile))
+
+
+def masterInfo(doc):
+    data = [
+        ["Name", "Patient Fullname",
+         "Date", "12/12/2016",
+         "Case #:", "168698"],
+        ["Room #:", "OPD", "Age:", "5", "Gender:", "Female"],
+        ["Requesting Physician:", "Dr. Lagare"]
+    ]
+    # data = [["%X" % (y*16+x) for x in range(12)] for y in range(3)]
+    data = [["" for x in range(12)] for y in range(3)]
+    styles = getSampleStyleSheet()
+    sN = styles["Normal"]
+    sN.spaceBefore = 0
+    sN.spaceAfter = 2
+    # sN.leading = 0
+    # Row 1
+    data[0][0] = Paragraph("<b>Name:</b><u> Pelicano, Rochelle</u>", sN)
+    data[0][6] = Paragraph("<b>Date:</b><u> 06/02/2016</u>", sN)
+    data[0][10] = Paragraph("<b>Case #:</b><u> 145591</u>", sN)
+    # Row 2
+    data[1][0] = Paragraph("<b>Room #:</b><u> OPD</u>", sN)
+    data[1][4] = Paragraph("<b>Age:</b><u> 5</u>", sN)
+    data[1][8] = Paragraph("<b>Sex:</b><u> Female</u>", sN)
+    # Row 3
+    data[2][0] = Paragraph("<b>Requesting Physician:</b>" +
+                           "<u> Dr. Lagarei</u>", sN)
+
+    width, height = doc.pagesize
+    w = width - (doc.leftMargin * 2)
+    t = Table(data, colWidths=[w/12]*12)
+    t.setStyle(TableStyle([
+        ('GRID', (0, 0), (-1, -1), 1, black),
+        ('TOPPADDING', (0, 0), (-1, -1), 0),
+        ('BOTTOMPADDING', (0, 0), (-1, -1), 0),
+        # Row 1
+        ('SPAN', (0, 0), (5, 0)),
+        ('SPAN', (6, 0), (9, 0)),
+        ('SPAN', (10, 0), (11, 0)),
+        # Row 2
+        ('SPAN', (0, 1), (3, 1)),
+        ('SPAN', (4, 1), (7, 1)),
+        ('SPAN', (8, 1), (11, 1)),
+        # Row 3
+        ('SPAN', (0, 2), (11, 2)),
+    ]))
+    return t
 
 
 def test():
@@ -133,6 +187,7 @@ def test():
             leftIndent=1*cm
             )
 
+    doc = ReportTemplate('mintoc.pdf')
     # Build story.
     story = []
     toc = TableOfContents()
@@ -142,6 +197,8 @@ def test():
     # For conciseness we use the same styles for headings and TOC entries
     toc.levelStyle = [h1, h2]
 
+    width, height = doc.pagesize
+    story.append(masterInfo(doc))
     story.append(toc)
     story.append(PageBreak())
     story.append(Paragraph('First heading', h1))
@@ -154,7 +211,6 @@ def test():
     story.append(Paragraph('Last heading', h1))
     # story.append(header)
 
-    doc = ReportTemplate('mintoc.pdf')
     doc.multiBuild(story)
 
 
